4fa66749dce459c16ed3b27a06be4a0ade6bb762
Lisandro Jofré <lisandrojofre@gmail.com>
2016-09-03 15:43:22 -0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// Dependencies.
+var browserSync = require("browser-sync").create();
+var cssmin      = require("gulp-cssmin");
+var gulp        = require("gulp");
+var jade        = require("gulp-jade");
+var reset       = require("node-reset-scss");
+var sass        = require("gulp-sass");
+	
+/**
+ * Tasks
+ */
+
+gulp.task("index", function () {
+    return gulp.src("./src/index.jade")
+        .pipe(jade())
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("styles", function () {
+    return gulp.src("./src/styles.scss")
+        .pipe(sass({
+        	includePaths: reset.includePath
+        }))
+        .pipe(cssmin())
+        .pipe(gulp.dest("./assets/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("favicon", function () {
+    return gulp.src("./src/favicon.ico")
+        .pipe(gulp.dest("./assets/"))
+});
+
+gulp.task("build", ["index", "styles", "favicon"]);
+
+gulp.task("serve", ["build"], function () {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./src/index.jade", ["index"]);
+    gulp.watch("./src/styles.scss", ["styles"]);
+});
