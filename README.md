ee89ef20106f1b4c87f163d6ea93ea18f4db53a7
Lisandro Jofre <lisandro.jofre@attackiq.com>
2018-10-28 15:22:43 -0300

    Fix merge conflicts.

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # lisandro-jofre.github.io
 
-Personal resume.
+Personal resume for screen/print media.
 
-## Usage
+## Scripts
 
-- ```npm install``` to install the dependencies.
 - ```npm run build``` to build the project.
 - ```npm run serve``` to start a local web server that watches for file changes to rerender the project.
-
-## Browser Support
-Chrome | IE    | Firefox | Safari | Opera  
------- | ----- | ------- | ------ | -----
-4.0+   | 10.0+ | 3.5+    | 3.2+   | 10.5+
 
 ## License
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,39 @@
-const browserSync = require("browser-sync").create();
-const concat      = require("gulp-concat");
-const cssmin      = require("gulp-cssmin");
-const del         = require("del");
-const gulp        = require("gulp");
-const gulpif      = require("gulp-if");
-const ngHtml2Js   = require("gulp-ng-html2js");
-const ngAnnotate  = require("gulp-ng-annotate");
-const pug         = require("gulp-pug");
-const rename      = require("gulp-rename");
-const runSequence = require("run-sequence");
-const sass        = require("gulp-sass");
-const uglify      = require("gulp-uglify");
-const useref      = require("gulp-useref");
-
-const srcDir      = "./src";
-const buildDir    = "./build";
+// Dependencies.
+var browserSync = require("browser-sync").create();
+var cssmin      = require("gulp-cssmin");
+var gulp        = require("gulp");
+var jade        = require("gulp-jade");
+var reset       = require("node-reset-scss");
+var sass        = require("gulp-sass");
 	
-gulp.task("index", function(done) {
-    return gulp.src(`${srcDir}/index.pug`)
-        .pipe(pug({pretty: true}))
-        .pipe(useref())
-        .pipe(gulpif("*.js", ngAnnotate()))
-        .pipe(gulpif("*.js", uglify()))
+/**
+ * Tasks
+ */
+
+gulp.task("index", function () {
+    return gulp.src("./src/index.jade")
+        .pipe(jade())
         .pipe(gulp.dest("./"))
         .pipe(browserSync.stream());
 });
 
-gulp.task("partials", function () {
-    return gulp.src([
-        `!${srcDir}/index.pug`,
-        `${srcDir}/**/*.pug`
-    ])
-    .pipe(pug())
-    .pipe(ngHtml2Js({
-        moduleName: "app.partials",
-        rename: function(path) {
-            let pathParts = path.split("/");
-            return "partials/" + pathParts[pathParts.length - 1];
-        }
-    }))
-    .pipe(concat("partials.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest(`${buildDir}/partials`))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("assets", function() {
-    return gulp.src([
-        `!${srcDir}/assets/scss/**/*.*`,
-        `${srcDir}/assets/**/*.*`
-    ])
-    .pipe(gulp.dest(`${buildDir}/assets`))
-});
-
-gulp.task("vendors", function() {
-    return gulp.src(`${srcDir}/vendors/*.js`)
-        .pipe(gulp.dest(`${buildDir}/vendors/`));
-});
-
 gulp.task("styles", function () {
-    return gulp.src(`${srcDir}/assets/scss/index.scss`)
-        .pipe(rename("bundle.scss"))
-        .pipe(sass())
+    return gulp.src("./src/styles.scss")
+        .pipe(sass({
+        	includePaths: reset.includePath
+        }))
         .pipe(cssmin())
-        .pipe(gulp.dest(`${buildDir}/`))
+        .pipe(gulp.dest("./"))
         .pipe(browserSync.stream());
 });
 
-gulp.task("clear", function() {
-    del([`${buildDir}/`, "./index.html"]);
-});
-
-gulp.task("build", function(done) {
-    runSequence("clear", "partials", "styles", "vendors", "index", "assets", done);
-});
+gulp.task("build", ["index", "styles"]);
 
 gulp.task("serve", ["build"], function () {
     browserSync.init({
         server: "./"
     });
 
-    gulp.watch(`${srcDir}/index.pug`, ["index"]);
-    
-    gulp.watch([`!${srcDir}/vendors/**/*.*`, `${srcDir}/**/*.pug`], function() {
-        runSequence("partials", "index");
-    });
-    
-    gulp.watch([`!${srcDir}/vendors/**/*.*`, `!${srcDir}/**/*.spec.js`, `${srcDir}/**/*.js`], ["index"]);
-    
-    gulp.watch([`!${srcDir}/vendors/**/*.*`, `${srcDir}/**/*.scss`], function() {
-        runSequence("styles", "index");
-    });
-
-    gulp.watch([`${srcDir}/assets/**/*.*`], ["assets"]);
+    gulp.watch("./src/index.jade", ["index"]);
+    gulp.watch("./src/styles.scss", ["styles"]);
 });
